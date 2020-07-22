25b5c087bda6348841df279600bd51f805769fd8
Vincent Huynh <vin.huynh1@gmail.com>
2020-07-21 20:09:43 -0500

    Updates

---
+++ precache-manifest.f1356e0b8ce2e756c19a83d7ad01ebda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4ce956e0d7bcc53ca57460b58e3a67",
+    "url": "/lofigen/index.html"
+  },
+  {
+    "revision": "38bd7da14a116eb90791",
+    "url": "/lofigen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2b1f42a86e784cf275a",
+    "url": "/lofigen/static/js/2.8c30a941.chunk.js"
+  },
+  {
+    "revision": "e58b2fed1a83725c5b0108382043ede5",
+    "url": "/lofigen/static/js/2.8c30a941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38bd7da14a116eb90791",
+    "url": "/lofigen/static/js/main.2c60c5c1.chunk.js"
+  },
+  {
+    "revision": "7c9d01d445a19f18936b",
+    "url": "/lofigen/static/js/runtime-main.0868bb00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lofigen/static/media/logo.5d5d9eef.svg"
+  }
+]);
