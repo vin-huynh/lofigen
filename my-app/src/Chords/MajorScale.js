3cf283359dc4c74d82cefc83df4c70484b2b3f88
Vincent Huynh <vin.huynh1@gmail.com>
2020-07-26 14:59:08 -0500

    changed major scale exports

--- my-app/src/Chords/MajorScale.js
+++ my-app/src/Chords/MajorScale.js
@@ -1,11 +1,4 @@
-const singleOct = [0,2,4,5,7,9,11];
-const doubleOct = [...singleOct, ...singleOct.map(n=>n+12)];
-const tripleOct = [...doubleOct, ...singleOct.map(n=>n+24)];
-const fiveToFive = [...singleOct.map(n=>n-12).slice(4), ...singleOct, ...singleOct.map(n=>n+12).slice(0,5)]
-
-module.exports = {
-    singleOct: singleOct,
-    doubleOct: doubleOct,
-    tripleOct: tripleOct,
-    fiveToFive: fiveToFive,
-};
+export const singleOct = [0,2,4,5,7,9,11];
+export const doubleOct = [...singleOct, ...singleOct.map(n=>n+12)];
+export const tripleOct = [...doubleOct, ...singleOct.map(n=>n+24)];
+export const fiveToFive = [...singleOct.map(n=>n-12).slice(4), ...singleOct, ...singleOct.map(n=>n+12).slice(0,5)]
